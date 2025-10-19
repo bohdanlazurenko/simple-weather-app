081a1a4c0b1c23fd8182ddd571f46b4ac0ec29f0
bohdanlazurenko <142092646+bohdanlazurenko@users.noreply.github.com>
2025-10-19 02:50:09 +0200

    Add lib/types.ts

---
+++ lib/types.ts
@@ -0,0 +1,10 @@
+export interface WeatherData {
+  city: string
+  country: string
+  temperature: number
+  description: string
+  icon: string
+  humidity: number
+  windSpeed: number
+  feelsLike: number
+}
